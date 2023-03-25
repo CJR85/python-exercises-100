918522555d54b7add20432932231de1f88ae9130
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-25 22:02:07 +0000

    Data Types

--- data_types.py
+++ data_types.py
@@ -1,6 +1,12 @@
 # Don't change the code below
-two_digit_number = input("Type a two digit number")
+two_digit_number = input("Type a two digit number: ")
 # Don't change the code above
 
 ################################
 # Write code below this line
+
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+
+result = int(first_digit) + int(second_digit)
+print(result)
