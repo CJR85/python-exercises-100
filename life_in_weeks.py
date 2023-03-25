3ec88a0d4e28a717264bc751224efb4be2bc7a94
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-25 22:43:05 +0000

    Life in weeks solution

--- life_in_weeks.py
+++ life_in_weeks.py
@@ -3,3 +3,13 @@ age = input("What is your current age?")
 # Don't change the code above
 
 # Write code below
+
+age_as_int = int(age)
+
+years_remaining = 90 - age_as_int
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+
+message = f"You have {days_remaining} days, {weeks_remaining} weeks, {months_remaining} months"
+print(message)
