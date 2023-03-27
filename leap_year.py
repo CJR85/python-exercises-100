caf8b74af8763f2367781d3a4763299dde4a0cc3
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-28 00:59:35 +0100

    Leap year

--- leap_year.py
+++ leap_year.py
@@ -5,3 +5,18 @@ year = int(input("Which year do you want to check?"))
 # Don't change the code above
 
 # Write code below
+
+# Is year divisble by 4?
+if year % 4 == 0:
+    # Is year divisble by 100?
+    if year % 100 == 0:
+        # Is year divisble by 400?
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+      print("Leap year.")
+else:
+  print("Not leap year.")
+
