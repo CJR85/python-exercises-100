63f9ea9e7be1b4a4916dd948593f69d1c3e08989
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-27 23:51:50 +0100

    Modulo Operator - Odd or Even

--- odd_or_even.py
+++ odd_or_even.py
@@ -3,3 +3,7 @@ number = int(input("Which number do you want to change?"))
 # Don't change the code above
 
 # Write code below
+if number % 2 == 0:
+    print("This is a even number.")
+else:
+    print("This is an odd number.")
