4e484c353c18cf4d9e431c1447d294d382dc80de
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-28 00:48:48 +0100

    Initial commit

---
+++ leap_year.py
@@ -0,0 +1,7 @@
+# Write a program that works out whether if a given year is a leap year. Normal year = 365, Leap year = 366
+
+# Don't change the code below
+year = int(input("Which year do you want to check?"))
+# Don't change the code above
+
+# Write code below
