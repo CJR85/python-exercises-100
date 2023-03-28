c41af69c58382a6a567437927537097660db86cb
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-28 01:17:19 +0100

    Initial commit

---
+++ pizza_order.py
@@ -0,0 +1,13 @@
+# Based on the user's order, work out the final bill.
+# Small pizza: $15, Medium pizza: $20 & Large pizza: $25
+# Pepperoni for small pizza: +$2, Pepperoni for medium & large pizza: +$3
+# Extra cheese for any size pizza: + $1
+
+# Don't change code below
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza would you like? S, M or L")
+add_peperoni = input("Would you like pepperoni? Y or N")
+extra_cheese = input("Do you want extra cheese? Y or N")
+# Don't change code above
+
+# Write code below
