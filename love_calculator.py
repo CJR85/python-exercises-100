4fa78ff3f499428a4655fecc7bfefd96e1db6c63
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-28 01:44:47 +0100

    Initial commit

---
+++ love_calculator.py
@@ -0,0 +1,10 @@
+# Write a program that tests the compatibility between two people.
+# To work this out, take both people's names and check the number of times the word TRUE occurs. Then check the number of times the word LOVE occurs. Then combine to get a 2 digit number.
+
+# Don't change the code below
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# Don't change the code above
+
+# Write code below
