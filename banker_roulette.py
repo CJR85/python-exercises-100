58ec464ad8c935146aa58dae4d28aa7a98130eab
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-29 22:28:25 +0100

    Banker Roulette solution

--- banker_roulette.py
+++ banker_roulette.py
@@ -1,8 +1,14 @@
 # Write a program which will select a random name from a list of names. The person picked will have to pay for everybody's food bill.
-
+import random
 # Split string method
 names_string = input("Give me every name, separated by a comma.")
 names = names_string.split(",")
 # Don't change the code above
 
 # Write code below
+# Get the total number of items in the list
+num_items = len(names)
+
+random_choice = random.randint(0, num_items - 1)
+bill_payer = names[random_choice]
+print(bill_payer + " is going to pay for the entire meal today.")
