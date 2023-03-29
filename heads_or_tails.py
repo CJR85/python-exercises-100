38efa94cb79dd66c5f1f5d4db597cdd7d7e65d4c
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-29 21:49:09 +0100

    Initial commit

---
+++ heads_or_tails.py
@@ -0,0 +1,5 @@
+# Write a virtual coin toss program.
+# It will randomly tell the user "Heads" or "Tails". First letter should be capitalized
+
+# Write code below
+# Hint: Remaember to import the random module first
