7ee1f03788251f2b65f1a286fb125831cea687e1
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-29 23:02:00 +0100

    Initial commit

---
+++ treasure_map.py
@@ -0,0 +1,11 @@
+# Instructions: Create a program which will mark a spot with an X.
+# Program should allow you to enter the position of the treasure using a two-digit system. The first digit is the horizontal column number, the second digit is the vertical row number.
+
+# 🚨 Don't change code below
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change code above
