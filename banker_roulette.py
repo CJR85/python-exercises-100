96c43742982f16bcaece8402cb38c1b675be4a30
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-29 22:13:56 +0100

    Initial commit

---
+++ banker_roulette.py
@@ -0,0 +1,8 @@
+# Write a program which will select a random name from a list of names. The person picked will have to pay for everybody's food bill.
+
+# Split string method
+names_string = input("Give me every name, separated by a comma.")
+names = names_string.split(",")
+# Don't change the code above
+
+# Write code below
