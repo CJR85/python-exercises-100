67a8805f8d8afa157d9eee5fc10f7528d27775f0
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-30 22:54:58 +0100

    Initial commit

---
+++ fizzbuzz.py
@@ -0,0 +1,6 @@
+# Write a program that prints each number betweeen 1 to 100 in turn.
+# When the number is divisible by 3, then instead of printing the number, it should print "Fizz".
+# When the number is divisible by 5, the instead of printing the number, it should print "Buzz".
+# When the number is divisible by both 3 and 5, the instead of printing the number, it should print "FizzBuzz".
+
+# Write code below
