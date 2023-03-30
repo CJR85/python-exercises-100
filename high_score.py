fc346947ad2b26a494866dfa4510734824db53b6
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-30 22:12:42 +0100

    Initial commit

---
+++ high_score.py
@@ -0,0 +1,13 @@
+# Write a program that calculates the highest score for each student in the class.
+# Important: Do not use the max or min functions.
+# Output must match the example: 
+# "The highest score in the class is: X"
+
+# 🚨 Don't change the code below
+student_scores = input("Input a list of student scores")
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above
+
+# Write code solution below
