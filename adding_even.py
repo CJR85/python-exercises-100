cb1d9fb0a7d1ac9223035cb47547357650e324ef
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-30 22:42:16 +0100

    Initial commit

---
+++ adding_even.py
@@ -0,0 +1,4 @@
+# Write a program that calculates the sum of all the even numbers from 1 to 100, including 1 and 100.
+# Important: There should only be ONE print statement in the console output
+
+# Write code below
