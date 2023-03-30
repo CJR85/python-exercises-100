ac30080d2e8e694c38a4f94030c17c3c4675d327
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-30 23:01:06 +0100

    FizzBuzz solution

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,3 +4,16 @@
 # When the number is divisible by both 3 and 5, the instead of printing the number, it should print "FizzBuzz".
 
 # Write code below
+
+for number in range(1, 101):
+    # Divisible by 3 and 5
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    # Divisible by 3
+    elif number % 3 == 0:
+        print("Fizz")
+    # Divisible by 5
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
