e2fa5b8bc104cee2ac243f4fe4944bf884ad59be
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-30 22:47:37 +0100

    Adding even - solution 2

--- adding_even.py
+++ adding_even.py
@@ -8,3 +8,9 @@ total = 0
 for number in range(2, 101, 2):
     total += number
 print(total)
+# Solution 2
+total2 = 0
+for number in range(1, 101):
+    if number % 2 == 0:
+        total2 += number
+print(total2)
