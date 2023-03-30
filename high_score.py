2022bf9e456b58db43e0868338ae8dadbf65f97c
Calvin Roberts <roberts.calvin85@gmail.com>
2023-03-30 22:33:05 +0100

    High score solution

--- high_score.py
+++ high_score.py
@@ -4,10 +4,15 @@
 # "The highest score in the class is: X"
 
 # 🚨 Don't change the code below
-student_scores = input("Input a list of student scores")
+student_scores = input("Input a list of student scores: ").split()
 for n in range(0, len(student_scores)):
     student_scores[n] = int(student_scores[n])
 print(student_scores)
 # 🚨 Don't change the code above
 
 # Write code solution below
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+print(f"The highest score in the class is: {highest_score}")
