a96fb9667c164ae675203c80ab977f0e1190846f
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-03 23:07:21 +0100

    Prime number checker

---
+++ prime_number_checker.py
@@ -0,0 +1,16 @@
+# Instructions: Prime numbers are numbers that can only be cleanly divisble by itself & 1.
+# You need to write a function that checks whether if the number passed into it is a prime number or not.
+
+# e.g. 2 is a prime number because it is only divisible by 1 & 2.
+# But 4 is not because you can didvide it by 1, 2 or 4.
+
+# Write your code below
+
+
+
+
+# Write your code above
+
+# Do NOT change any of the code below
+n = int(input("Check this number: "))
+prime_checker(number=n)
