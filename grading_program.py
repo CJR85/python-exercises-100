7055fea6a5e8a968ef1f126080aa12dffb51e4d9
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-05 14:20:20 +0100

    Grading program solution

--- grading_program.py
+++ grading_program.py
@@ -8,11 +8,19 @@ student_scores = {
 # Don't change the code above
 
 # TODO-1: Create an empty dictionary called student_grades.
-
+student_grades = {}
 
 # TODO-2: Write code below to add the grades to student_grades.
-
-
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif score > 80:
+        student_grades[student] = "Exceeds Expectations"
+    elif score > 70:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
 
 # Don't change the code below.
 print(student_grades)
