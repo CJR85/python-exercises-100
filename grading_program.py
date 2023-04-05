513ae6e4d42eb695bdb9edf744b41a00ce0dba7a
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-05 14:12:24 +0100

    Initial commit

---
+++ grading_program.py
@@ -0,0 +1,18 @@
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+# Don't change the code above
+
+# TODO-1: Create an empty dictionary called student_grades.
+
+
+# TODO-2: Write code below to add the grades to student_grades.
+
+
+
+# Don't change the code below.
+print(student_grades)
