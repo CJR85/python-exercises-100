72f10cdab2361130d28349ff5c09d9214d194849
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-05 15:44:54 +0100

    Initial commit

---
+++ dict_in_lists.py
@@ -0,0 +1,25 @@
+# Instructions: Create a program that add to the travel_log.
+# Write a function that will add the entry for Russia to the travel_log.
+
+
+travel_log = [
+    {
+      "country": "France",
+      "visits": 12,
+      "cities": ["Paris", "Lille", "Dijon"]
+    },
+    {
+      "country": "Germany",
+      "visits": 5,
+      "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    },
+]
+# DO NOT change the code above
+
+# TODO: Write a function that will allow new countries to be added to the travel_log.
+
+
+
+# Do not change the code below
+add_new_country("Russia", 2, "Moscow", "Saint Petersburg")
+print(travel_log)
