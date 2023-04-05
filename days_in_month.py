6bdde552c2023c66ba6cbbf0b29c7a18fa753408
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-05 23:35:36 +0100

    Days in month solution

--- days_in_month.py
+++ days_in_month.py
@@ -1,9 +1,6 @@
 # Instructions: Convert the function is_leap_year() so that instead of printing "Leap year" or "Not leap year", it should return TRUE if it IS a leap year & return FALSE otherwise.
 # Then create a function called days_in_month() which will take the year & month as inputs.
 
-# Don't change the code below
-year = int(input("Which year do you want to check?"))
-# Don't change the code above
 
 # Write code below
 
@@ -13,16 +10,22 @@ def is_leap_year(year):
       if year % 100 == 0:
           # Is year divisble by 400?
           if year % 400 == 0:
-              print("Leap year.")
+              return True
           else:
-              print("Not leap year.")
+              return False
       else:
-        print("Leap year.")
+        return True
   else:
-    print("Not leap year.")
+    return False
 
-def days_in_month():
+def days_in_month(year, month):
+  #  if month > 12 or month < 1:
+  #     return "Invalid month"
    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+   if is_leap_year(year) and month == 2:
+      return 29
+   return month_days[month - 1]
+      
 
 # DO NOT change the code below
 year = int(input("Enter a year: "))
