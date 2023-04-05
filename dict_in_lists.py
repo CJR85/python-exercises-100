a56095fb15919b03c3a7bc1aa3b9b148fdd38ef4
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-05 15:53:32 +0100

    Dictionary in list solution

--- dict_in_lists.py
+++ dict_in_lists.py
@@ -17,9 +17,14 @@ travel_log = [
 # DO NOT change the code above
 
 # TODO: Write a function that will allow new countries to be added to the travel_log.
-
+def add_new_country(country_visited, times_visited, cities_visited):
+    new_country = {}
+    new_country["country"] = country_visited
+    new_country["visits"] = times_visited
+    new_country["cities"] = cities_visited
+    travel_log.append(new_country)
 
 
 # Do not change the code below
-add_new_country("Russia", 2, "Moscow", "Saint Petersburg")
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
 print(travel_log)
