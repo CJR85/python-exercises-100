e4582b8fc28c3599a5f74ef13b937d3218c4e435
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-08 12:58:59 +0100

    Initial commit

---
+++ odd_or_even_debug.py
@@ -0,0 +1,8 @@
+# Instructions: Read the code below, spot the problem and modify the code to fix the program.
+
+number = int(input("Which number do you want to check? "))
+
+if number % 2 = 0:
+    print("This is a even number.")
+else:
+    print("This is an odd number.")
