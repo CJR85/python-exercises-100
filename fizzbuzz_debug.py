3b5936247376b17aa21aac94bb5faba2f557199b
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-08 13:26:18 +0100

    Debugged code solution

--- fizzbuzz_debug.py
+++ fizzbuzz_debug.py
@@ -1,11 +1,11 @@
 # Instructions: Read the code below, spot the problem and modify the code to fix the program.
 
 for number in range(1, 100):
-    if number % 3 == 0 or number % 5 == 0:
+    if number % 3 == 0 and number % 5 == 0: #Changed 'or' to 'and'
         print("FizzBuzz")
-    if number % 3 == 0:
+    elif number % 3 == 0: #Changed if to elif 
         print("Fizz")
-    if number % 5 == 0:
+    elif number % 5 == 0: #Changed if to elif
         print("Buzz")
     else:
-        print([number])
+        print(number) #Removed square brackets
