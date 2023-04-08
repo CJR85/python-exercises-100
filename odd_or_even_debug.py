0da6b5b32b66a828f4932d0831c94e6937e856a0
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-08 13:02:45 +0100

    Debugged solution

--- odd_or_even_debug.py
+++ odd_or_even_debug.py
@@ -2,7 +2,7 @@
 
 number = int(input("Which number do you want to check? "))
 
-if number % 2 = 0:
+if number % 2 == 0: #Add two equals
     print("This is a even number.")
 else:
     print("This is an odd number.")
