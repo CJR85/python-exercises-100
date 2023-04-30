1abcf0fdbf95bc65cb6422c129f0826fc58e7b0f
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-30 16:03:45 +0100

    Initial commit

---
+++ squaring_numbers.py
@@ -0,0 +1,10 @@
+# Write a List Comprehension to create a new list called squared_numbers. This new list should contain every number in the numbers list but each number should be squared.
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# Do NOT change code above
+
+# Write code below:
+
+# Write code above
+
+# print(squared_numbers)
