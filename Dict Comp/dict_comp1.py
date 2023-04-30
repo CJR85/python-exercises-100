911944acb60cc64168052b4b8ba7cc8e5aad17ef
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-30 17:02:15 +0100

    Initial commit: Dictionary Comprehension

---
+++ Dict Comp/dict_comp1.py
@@ -0,0 +1,9 @@
+# TODO: Use Dictionary Comprehension to create a dictionary called result that takes each word in the given sentence and calculates the number of letters in each word.
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# Do NOT change the code above
+
+# Write code below
+
+
+print(result)
