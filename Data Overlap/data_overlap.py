cfe2afd3c9d1b31bfed8b68d31395feb1c8d5b7a
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-30 16:23:29 +0100

    Initial commit

---
+++ Data Overlap/data_overlap.py
@@ -0,0 +1,9 @@
+# TODO: Inside file1.txt & file2.txt are a bunch of numbers that are on single lines.
+# Create a list called result which will contain the common numbers in both files.
+
+
+# Write code below
+
+# Write code above
+
+print(result)
