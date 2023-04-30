d1c32bbde445be06b95dd3465d6b80994d914716
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-30 17:07:49 +0100

    Dictionary Comprehension solution

--- Dict Comp/dict_comp1.py
+++ Dict Comp/dict_comp1.py
@@ -4,6 +4,6 @@ sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
 # Do NOT change the code above
 
 # Write code below
-
+result = {word:len(word) for word in sentence.split()}
 
 print(result)
