2121888bc3e7896eec810ad7a32535924ec4bbdf
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-30 16:06:25 +0100

    Squared numbers solution

--- squaring_numbers.py
+++ squaring_numbers.py
@@ -4,7 +4,7 @@ numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 # Do NOT change code above
 
 # Write code below:
-
+squared_numbers = [num * num for num in numbers]
 # Write code above
 
-# print(squared_numbers)
+print(squared_numbers)
