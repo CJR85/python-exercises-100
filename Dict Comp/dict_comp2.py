515391c87f0364da0bd083f6afb8125f57f862e2
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-30 17:18:37 +0100

    Initial commit: Dictionary Comprehension 2

---
+++ Dict Comp/dict_comp2.py
@@ -0,0 +1,14 @@
+# TODO: Use Dictionary Comprehension to create a dictionary called weather_f which takes each temperature in degrees Celsius and converts into degrees Fahrenheit.
+# To convert temp_c to temp_f: (temp_c *9/5) + 32 = temp_f.
+
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+# Do NOT change code above
