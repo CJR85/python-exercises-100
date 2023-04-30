46a87b1141fe695d09b99551b6db9334f0336385
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-30 16:14:11 +0100

    Filtering Even Numbers solution

--- filter_even_num.py
+++ filter_even_num.py
@@ -4,7 +4,7 @@ numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 # Do NOT change code above
 
 # Write code below
-
+result = [num for num in numbers if num % 2 == 0]
 # Write code above
 
 print(result)
