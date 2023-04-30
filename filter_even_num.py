3a95b496167e2c62bb24a2388f936fba9912329a
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-30 16:10:51 +0100

    Initial commit

---
+++ filter_even_num.py
@@ -0,0 +1,10 @@
+# TODO: Write a List Comprehension to create a new list called result. This new list should contain the even numbers from the list.
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# Do NOT change code above
+
+# Write code below
+
+# Write code above
+
+print(result)
